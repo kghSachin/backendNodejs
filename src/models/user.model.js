937416b28583b50733a63837bfdd5acc64ba300d
sachin <118202383+kghSachin@users.noreply.github.com>
2024-01-11 09:36:11 +0545

    jwt tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,5 +55,27 @@ userSchema.pre("save", async function(next){
 userSchema.methods.isPasswordCorrect=async function(password){
 return await bcrypt.compare(password,this.password)
 }
-
+userSchema.methods.generateAccessToken= function(){
+return jwt.sign({
+    _id : this._id,
+    email:this.email,
+    userName:this.userName,
+    fullName: this.fullName,
+},
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+}
+)
+}
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign({
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
 export const User = mongoose.model("User", userSchema);
