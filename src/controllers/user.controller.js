3dd4a7913795b7daef6d1b2e0676867d3906be69
sachin <118202383+kghSachin@users.noreply.github.com>
2024-01-12 22:35:39 +0545

    logic building theory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,19 @@ import { asyncHandler } from "../utils/async-handler.js"
 
 
 const registerUser = asyncHandler( async(req, res)=>{
-  return res.status(201).json({message:"OK"})
+  // return res.status(201).json({message:"OK"})
+  //Get user details form the user 
+  // validation 
+  // check duplication 
+  // check for the images or avatar 
+  // if avatar exist upload them to cloudinary 
+  // check if the avatar is uploaded or not 
+  //create user objects 
+  // create entry in database 
+  // filter password and refresh token from response 
+  //check user creation 
+  //return res
+  
 } )
 
 
