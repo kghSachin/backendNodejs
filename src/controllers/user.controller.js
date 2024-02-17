53a51167c3ffd8b295620d8c0fc61c4f3f9fb51e
sachin <118202383+kghSachin@users.noreply.github.com>
2024-02-17 21:51:16 +0545

    fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -120,10 +120,10 @@ const loginUser = asyncHandler(async (req, res)=>{
   return res.status(200).cookie("accessToken",accessToken, options).cookie("refreshToken",refreshToken,options).json(
     new ApiResponse(
       200,
+      "user logged in successfully",
       {
         user: loggedInUser, accessToken,refreshToken
-      },
-      "user logged in successfully"
+      }
     )
   )
 
